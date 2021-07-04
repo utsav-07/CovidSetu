582ad09f2623f6293ae04c156f8ceba476cb2bd0
Suraj Prasad Soni <Surajsoni119@gmail.com>
2021-07-04 21:25:30 +0530

    location changes

--- app/src/main/java/com/example/register_login/Broadcast.kt
+++ app/src/main/java/com/example/register_login/Broadcast.kt
@@ -1,9 +1,15 @@
 package com.example.register_login
 
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.coroutines.CoroutineScope
@@ -11,6 +17,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class Broadcast : AppCompatActivity() {
+
+     lateinit var location: Location
+    lateinit var  fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_broadcast)
@@ -19,16 +28,28 @@ class Broadcast : AppCompatActivity() {
         var Topic = "/topics/$pin"
         FirebaseMessaging.getInstance().subscribeToTopic(Topic)
         var broadCastBtn=findViewById<Button>(R.id.btnBroadCast)
-        var tit = findViewById<TextInputEditText>(R.id.edTitleTxt)
-        var msg = findViewById<TextInputEditText>(R.id.edtxtMsg)
+        val tit = findViewById<TextInputEditText>(R.id.edTitleTxt)
+        val msg = findViewById<TextInputEditText>(R.id.edtxtMsg)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         broadCastBtn.setOnClickListener {
+            fetchLocation()
+
             val title = tit.text.toString()
             val message = msg.text.toString()
             //TODO:work to be done
+            var longitude:String="1"
+            var latitude:String="2"
+            if(location.latitude!=null) {
+                latitude = location.latitude.toString()
+                longitude = location.longitude.toString()
+            }
+            Log.d("BroadCast","${location.longitude.toString()} ,${location.longitude.toString()}");
+
+
             val mob:String="9258441169"
             val name:String="Ajay Singh"
-            val latitude:Double=1.043.toDouble()
-            val longitude:Double=3.44.toDouble()
+
 
             if(title.isNotEmpty()&&message.isNotEmpty())
             {
@@ -40,6 +61,29 @@ class Broadcast : AppCompatActivity() {
 
         }
     }
+
+    private fun fetchLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+
+       if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!=
+           PackageManager.PERMISSION_GRANTED&&ActivityCompat
+               .checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+       {
+           ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),101)
+           return
+       }
+       task.addOnSuccessListener {
+           if(it!=null)
+           {
+               Toast.makeText(applicationContext,"${it.latitude}  ${it.longitude}",Toast.LENGTH_SHORT).show()
+
+
+           }
+
+       }
+       location=task.result
+    }
+
     private fun sendNotification(notification:pushNotification)= CoroutineScope(Dispatchers.IO).launch {
         try{
             val response = Retrofitinstance.api.postNotification(notification)
--- app/src/main/java/com/example/register_login/NotificationData.kt
+++ app/src/main/java/com/example/register_login/NotificationData.kt
@@ -1,4 +1,4 @@
 package com.example.register_login
 
-data class NotificationData(val title:String ,val message:String,val mob:String,val name:String,val longitude:Double,val latitude:Double)
+data class NotificationData(val title:String ,val message:String,val mob:String,val name:String,val longitude:String,val latitude:String)
 //TODO : work to be done
